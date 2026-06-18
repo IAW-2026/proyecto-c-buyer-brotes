362ae1f1f812e46926795294f1cc5d203a99fff2
Valentin Villarreal <lucasvalentin.villarreal@gmail.com>
2026-06-18 18:38:14 -0300

    Ahora las ordenes se caducan en 5 minutos.

--- app/lib/cancelarOrdenesCaducadas.ts
+++ app/lib/cancelarOrdenesCaducadas.ts
@@ -1,13 +1,13 @@
 import { prisma } from './prisma'
 
 export async function cancelarOrdenesCaducadas(buyerId: number) {
-  const unaHoraAtras = new Date(Date.now() - 60 * 60 * 1000)
+  const cincoMinutosAtras = new Date(Date.now() - 5 * 60 * 1000)
 
   await prisma.order.updateMany({
     where: {
       buyer_id: buyerId,
       estado: 'pendiente',
-      created_at: { lt: unaHoraAtras }
+      created_at: { lt: cincoMinutosAtras }
     },
     data: { estado: 'caducada' }
   })
